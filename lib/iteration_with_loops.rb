f8bc196cb0b5423b31288c582b91c5cd2641ce96
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-29 00:29:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     element_index = 0 
     while element_index < src[row_index].count
     if src[row_index][element_index].class == "".class
-      cool_string << src[row_index][element_index] 
+      cool_string << src[row_index][element_index] << " "
       end
       element_index += 1 
     end
